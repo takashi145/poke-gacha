69e01a1fffda673bfe783b59f1b6edef3c560d21
takashi145 <t.shuhei0203@gmail.com>
2023-12-31 20:22:49 +0900

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import GachaListVue from './GachaList.vue'
 import HomeVue from './Home.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeVue },
-  { path: '/gacha', name: 'Gacha', component: GachaListVue },
 ]
 
 const router = createRouter({
